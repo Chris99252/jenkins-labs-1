d5201958c380d1646d8172b241b8e6d47135e22a
chris <s86610000@gmail.com>
2016-04-10 14:53:10 +0800

    GitHub WebHook Trigger api token

--- Presentation/Nop.Web/Controllers/BackInStockSubscriptionController.cs
+++ Presentation/Nop.Web/Controllers/BackInStockSubscriptionController.cs
@@ -12,7 +12,7 @@ using Nop.Web.Models.Common;
 namespace Nop.Web.Controllers
 {
     /// <summary>
-    /// GitHub WebHook Trigger 2
+    /// GitHub WebHook Trigger 3
     /// </summary>
     public partial class BackInStockSubscriptionController : BasePublicController
     {

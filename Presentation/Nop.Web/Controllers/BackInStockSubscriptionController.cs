e05b8d5cf8f1be0e7c3a73b4fab25576a8dfaa46
chris <s86610000@gmail.com>
2016-04-10 15:15:51 +0800

    GitHub WebHook Trigger 5

--- Presentation/Nop.Web/Controllers/BackInStockSubscriptionController.cs
+++ Presentation/Nop.Web/Controllers/BackInStockSubscriptionController.cs
@@ -12,7 +12,7 @@ using Nop.Web.Models.Common;
 namespace Nop.Web.Controllers
 {
     /// <summary>
-    /// GitHub WebHook Trigger 4
+    /// GitHub WebHook Trigger 5
     /// </summary>
     public partial class BackInStockSubscriptionController : BasePublicController
     {
